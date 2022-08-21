167da77c378336ff1fe20c6919f4d6a4fb45fdd0
sarah almutari <sara0moha78@gmail.com>
2022-08-21 08:00:12 +0300

    .

--- script.js
+++ script.js
@@ -8,22 +8,32 @@ let random_grade_div = document.getElementById("random-grade");
 // لا تقم بتغيير او تخريب الكود الموجود فوق هذا الكومنت
 
 // طبّق الخطوة 4 في أسفل هذا الكومنت
-
+grades.pop()
 // طبّق الخطوة 5 في أسفل هذا الكومنت
-
+grades.push(99)
 // طبّق الخطوة 6 أسفل هذا الكومنت
 
+let gradesLength = grades.length;
+for (let i = 0 ; i < gradesLength; i++ ) {
+  all_grades_div.innerHTML += `<div>${grades[i]}</div>`;
+}
 
 
 function search() {
   let inputValue = document.getElementById("input").value;
-  
+  let filteredGrades = grades.filter((x) => x == inputValue);
   // طبّق الخطوة 7 أسفل هذا الكومنت
-  
+  if(inputValue == filteredGrades ){
+console.log(filteredGrades)
+  } else{
+    console.log(`الدرجة غير موجوده`)
+  }
+ 
 }
 
 // طبّق الخطوة 8 و 9 أسفل هذا الكومنت
+let randomIndex = Math.floor( Math.random() * grades.length)
 
 // طبّق الخطوة 10 أسفل هذا الكومنت
 
-
+random_grade_div.innerHTML = `<div>${grades[randomIndex]}</div>`;
